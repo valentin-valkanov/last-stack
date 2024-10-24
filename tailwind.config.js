869ef2c261e95af1f8b0aaf98edfa454cbd3a45f
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-10-24 14:02:32 +0300

    celebrate contoller added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
-const plugin = require('tailwinfcss/plugin')
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-    "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    plugin(function({ addVariant }) {
-      addVariant('turbo-frame', 'turbo-frame[src] &')
-    }),
-  ],
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+        "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        plugin(function({ addVariant }) {
+            addVariant('turbo-frame', 'turbo-frame[src] &')
+        }),
+    ],
 }
